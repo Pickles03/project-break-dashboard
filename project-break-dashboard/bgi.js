28b28d458c126dd4b30b2a38d601d5f2a4edcf0b
Pickles03 <elizaveta.kris@gmail.com>
2025-04-16 17:45:34 +0200

    started doing css, clock is more or less done

--- project-break-dashboard/bgi.js
+++ project-break-dashboard/bgi.js
@@ -1,11 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const bgi = ['bgi/bgi1.jpg', 'bgi/bgi2.jpg', 'bgi/bgi3.jpg', 'bgi/bgi4.jpg', 'bgi/bgi5.jpg', 'bgi/bgi6.jpg', 'bgi/bgi7.jpg', 'bgi/bgi8.jpg', 'bgi/bgi9.jpg', 'bgi/bgi10.jpg'];
+    const bgi = [{url: 'bgi/bgi1.jpg', tone: 'dark' }, 
+                {url: 'bgi/bgi2.jpg', tone: 'medium'},  
+                {url: 'bgi/bgi5.jpg', tone: 'light'}, 
+                {url: 'bgi/bgi6.jpg', tone: 'dark'}, 
+                {url: 'bgi/bgi7.jpg', tone: 'light'}, 
+                {url: 'bgi/bgi8.jpg', tone: 'medium'}, 
+                {url: 'bgi/bgi9.jpg', tone: 'dark'}, 
+                {url: 'bgi/bgi10.jpg', tone: 'medium'}
+            ];
 
     function randomizeBgi() {
         const randomBgi = Math.floor(Math.random() * bgi.length);
         const randomPic = bgi[randomBgi];
-        document.body.style.backgroundImage = `url('${randomPic}')`;
+        document.body.style.backgroundImage = `url('${randomPic.url}')`;
+
+        if (randomPic.tone === 'dark') {
+            document.body.style.color = 'white';
+        }
+        else if (randomPic.tone === 'light') {
+            document.body.style.color = 'black';
+        }
+        else {
+            document.body.style.color = 'dark-gray';
+        }
     }
+
 
     randomizeBgi();
 
--- project-break-dashboard/clock.js
+++ project-break-dashboard/clock.js
@@ -1,25 +1,37 @@
-/*
 
-<div id="clock" class="clock">
-        <p id="time" class="time">Time: </p>
-        <p id="date" class="date">Date: </p>
-    </div>
-
-*/
 
 currentTime = document.getElementById('time');
 currentDate = document.getElementById('date');
+const greeting = document.getElementById('greeting');
 
 const updateClock = () => {
     const clock = new Date ();
     let hours = clock.getHours();
     let minutes = clock.getMinutes();
     let seconds = clock.getSeconds();
-    hours = (hours < 10) ? "0" + hours : hours; //this adds a leading zero to the hours if it is less than 10
+
+    hours = (hours < 10) ? "0" + hours : hours; 
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
+
     const time = `${hours}:${minutes}:${seconds}`;
+
     currentTime.innerHTML = `${time}`;
+
+    if (hours >= 6 && hours < 10) {
+        greeting.innerHTML = "Good Morning!";
+    } else if (hours >= 10 && hours < 12) {
+        greeting.innerHTML = "Hope your day is going well!";
+    }
+    else if (hours >= 12 && hours < 14) {
+        greeting.innerHTML = "Maybe take a break?";
+    } else if (hours >= 14 && hours < 17) {
+        greeting.innerHTML = "That lunch was delicious!";
+    } else if (hours >= 17 && hours < 20) {
+        greeting.innerHTML = "Good Evening!";
+    } else {
+        greeting.innerHTML = "Good Night 🌙!";
+    }
 };
 setInterval(updateClock, 1000); //this will call the function every second
 
